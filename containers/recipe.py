fc3c93e20024909a3b8d71fc167c25dc30d5ce59
Scott McMillan <scott.andrew.mcmillan@gmail.com>
2021-08-21 14:35:07 -0500

    Update container recipe and Dockerfiles, add github action

--- containers/recipe.py
+++ containers/recipe.py
@@ -3,40 +3,67 @@
 
 Stage0 += comment('wassail container')
 
-image = USERARG.get('baseimage', 'ubuntu:18.04')
+image = USERARG.get('baseimage', 'ubuntu:20.04')
+toolset = USERARG.get('toolset', 'gnu')
+apt = []
 
-if image == 'ubuntu:18.04':
-  Stage0 += baseimage(image='ubuntu:18.04')
-elif image == 'ubuntu:16.04':
-  Stage0 += baseimage(image='ubuntu:16.04')
-elif image == 'centos:8':
-  Stage0 += baseimage(image='centos:8')
-elif image == 'centos:7':
-  Stage0 += baseimage(image='centos:7')
+if toolset == 'nvhpc':
+  Stage0 += baseimage(image='nvcr.io/nvidia/nvhpc:21.7-devel-cuda11.3-ubuntu20.04')
+  toolchain = hpccm.toolchain(CC='nvc', CXX='nvc++', CXXFLAGS='-O1')
+elif toolset == 'oneapi':
+  Stage0 += baseimage(image='intel/oneapi-hpckit:2021.3-devel-ubuntu18.04')
+  toolchain = hpccm.toolchain(CC='icx', CXX='icpx')
 else:
-  raise RuntimeError('unrecognized baseimage')
+  if image == 'ubuntu:20.04':
+    Stage0 += baseimage(image='ubuntu:20.04')
+    apt = ['libopenmpi-dev', 'openmpi-bin']
+  elif image == 'ubuntu:18.04':
+    Stage0 += baseimage(image='ubuntu:18.04')
+    apt = ['libopenmpi-dev', 'openmpi-bin', 'pciutils-dev']
+  elif image == 'ubuntu:16.04':
+    Stage0 += baseimage(image='ubuntu:16.04')
+    apt = ['libopenmpi-dev', 'openmpi-bin', 'pciutils-dev']
+  elif image == 'centos:8':
+    Stage0 += baseimage(image='centos:8')
+  elif image == 'rockylinux:8':
+    Stage0 += baseimage(image='rockylinux/rockylinux:8')
+  elif image == 'centos:7':
+    Stage0 += baseimage(image='centos:7')
+  else:
+    raise RuntimeError('unrecognized baseimage')
 
+  if toolset == 'gnu':
+    if image == 'centos:7':
+      compiler = gnu(fortran=False, version='7')
+    else:
+      compiler = gnu(fortran=False)
+  elif toolset == 'llvm':
+    compiler = llvm()
+  else:
+    raise RuntimeError('unrecognized toolset')
+
+  Stage0 += compiler
+  toolchain = compiler.toolchain
+
+# Python development environment
+Stage0 += python(python2=False, devel=True)
+
+# Development tools
 Stage0 += packages(ospackages=['autoconf', 'autoconf-archive', 'automake',
                                'ca-certificates', 'diffutils', 'file', 'git',
                                'libtool', 'make'],
                    powertools=True)
 
-Stage0 += python(python2=False, devel=True)
-
-if image == 'centos:7':
-  compiler = gnu(fortran=False, version='7')
-else:
-  compiler = gnu(fortran=False)
-Stage0 += compiler
-
-Stage0 += packages(apt=['libdispatch-dev', 'libibumad-dev', 'libpciaccess-dev',
-                        'libssh-dev', 'libudev-dev', 'pciutils-dev'],
+# wassail dependencies
+apt.extend(['libdispatch-dev', 'libibumad-dev', 'libpciaccess-dev',
+            'libssh-dev', 'libudev-dev'])
+Stage0 += packages(apt=apt,
                    yum=['libpciaccess-devel', 'libssh-devel', 'pciutils-devel',
                         'rdma-core-devel', 'systemd-devel', 'tbb'])
 
 Stage0 += generic_autotools(branch='master', check=True,
                             preconfigure=['./autogen.sh'],
                             repository='https://github.com/samcmill/wassail',
-                            toolchain=compiler.toolchain)
+                            toolchain=toolchain)
 Stage0 += environment(variables={'LD_LIBRARY_PATH':
                                  '/usr/local/lib:$LD_LIBRARY_PATH'})
