a82231e62c68a57b26c5eb7d633557c26475c15c
Scott McMillan <scott.andrew.mcmillan@gmail.com>
2026-07-20 20:53:28 -0500

    Some more cases reported by the Intel compiler

--- include/wassail/fmt/format.h
+++ include/wassail/fmt/format.h
@@ -2751,7 +2751,7 @@ constexpr auto operator""_a()
   return {};
 }
 #  else
-constexpr auto operator"" _a(const char* s, size_t) -> detail::udl_arg<char> {
+constexpr auto operator""_a(const char* s, size_t) -> detail::udl_arg<char> {
   return {s};
 }
 #  endif
@@ -2766,7 +2766,7 @@ constexpr auto operator"" _a(const char* s, size_t) -> detail::udl_arg<char> {
     std::string message = "The answer is {}"_format(42);
   \endrst
  */
-constexpr auto operator"" _format(const char* s, size_t n)
+constexpr auto operator""_format(const char* s, size_t n)
     -> detail::udl_formatter<char> {
   return {{s, n}};
 }
